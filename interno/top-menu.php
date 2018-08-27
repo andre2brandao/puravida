70c9bd3da5e9658cb03a6cdda918ab424c562a4a
André Brandão Cavalcanti Neto <42747025+andre2brandao@users.noreply.github.com>
2018-08-27 14:49:50 -0300

    Arquivos CSS e o Top-Menu.PHP

---
+++ interno/top-menu.php
@@ -0,0 +1,36 @@
+<!DOCTYPE HTML>
+<head>
+	<link rel="stylesheet" type="text/css" href="menu.css">
+</head>
+<body>
+	<?php
+		echo '
+
+		<ul>
+			<li><a href="index.php">Home/News</a></li>
+			<li><a href="?page=2">PGP Keys</a></li>
+			<li><a href="?page=3">Quem Somos</a></li>
+			<li><a href="?page=4">Termos</a></li>
+		
+		<div class="menu-esquerda">
+			<ul>
+				<li><a href="#carteira">Carteira</a></li>
+				<li><a href="cart.php">Cart</a></li>
+				<li><a href="#null">Usuário</a></li>
+
+				<li><a href="login.php">Login</a></li> 
+				<li><a href="register.php">Registre-se</a></li> 
+			</ul>
+		</div>
+
+	</ul>
+	<h1>PURA VIDA!</h1>
+	<div class="logo">
+		<h4>This is a paragraph.</h4>
+	</div>
+	
+
+	';
+?>
+</body>
+</html>
